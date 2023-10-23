2ec1c597e04029e779368cdfae4b243e540a4506
Ahmed Khaled Behary <99817000+ahmedbehary@users.noreply.github.com>
2023-10-23 14:57:16 +0300

    Add files via upload

---
+++ Part2 AVR Interfacing/main.c
@@ -0,0 +1,20 @@
+/*
+ * Application1.c
+ *
+ * Created: 20/10/2023 11:04:53 ص
+ * Author : Dell
+ */ 
+
+#include <avr/io.h>
+#include <avr/delay.h>
+//#define F_CPU 16000000UL
+#include "main.h"
+
+int main(void)
+{
+	
+    while(1) 
+    {
+		
+    }
+}
