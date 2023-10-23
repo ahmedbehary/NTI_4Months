7576dff66e29965555322787f5985fe35c3b64d0
Ahmed Khaled Behary <99817000+ahmedbehary@users.noreply.github.com>
2023-10-23 14:50:37 +0300

    Add files via upload

---
+++ Part2 AVR Interfacing/main.h
@@ -0,0 +1,19 @@
+﻿/*
+ * main.h
+ *
+ * Created: 20/10/2023 11:06:58 ص
+ *  Author: Dell
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+//#include "MCAL Layer/DIO/dio.h"
+#include "ECU Layer/LED/led.h"
+#include "MCAL Layer/std_types.h"
+#include "ECU Layer/PUSH BUTTON/button.h"
+#include "ECU Layer/LCD/lcd.h"
+#include "MCAL Layer/Interrupt/External Interrupt/External_Interrupt.h"
+#include "MCAL Layer/ADC/adc.h"
+
+#endif /* MAIN_H_ */
